5a9fbb066b572e8ff964d031d2e0b06ca98122ca
Ted Fowler <64517093+tfowl@users.noreply.github.com>
2021-11-17 17:16:47 -0500

    stats for mpg_data

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -27,7 +27,7 @@ export const allCarStats = {
       mpg_data.reduce((total, next) => total + next.highway_mpg, 0) /
       mpg_data.length,
   },
-  allYearStats: undefined,
+  allYearStats: getStatistics(mpg_data),
   ratioHybrids: undefined,
 };
 
